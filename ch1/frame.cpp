b03836fb3feb031141e44c7aee6339916eeb7535
amxmv1 <amxmv1@gmail.com>
2016-08-26 22:52:06 +0900

    first commit

---
+++ ch1/frame.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Your name:";
+	cin >> name;
+	const string greeting = "Hello " + name;
+	const string spaces(greeting.size(), ' ');
+	const string second = "* " + spaces + " *";
+	const string first(second.size(), '*');
+	cout << endl;
+	cout << first << endl;
+	cout << second << endl;
+	cout << "* " << greeting << " *" << endl;
+	cout << second << endl;
+	cout << first << endl;
+}
